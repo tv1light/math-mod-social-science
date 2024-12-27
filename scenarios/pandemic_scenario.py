b8f22c85fdf487187e37be021e464d684690bfdb
v.gutorov <v.gutorov@bft.ru>
2024-12-27 18:30:43 +0400

    Подправил сценарий пандемии

--- scenarios/pandemic_scenario.py
+++ scenarios/pandemic_scenario.py
@@ -1,28 +1,28 @@
 from .base_model import solve_system
 
 def pandemic_scenario():
-    # Параметры нормального сценария (до пандемии)
+    # Параметры нормального сценария (до катастрофы)
     normal_params = (1.085e-5, 6.51e-12, 8.2e-6)
-    catastrophe_params = (normal_params[0] // 3, normal_params[1] // 3, normal_params[2] // 3)  # Измененные параметры
+    catastrophe_params = (normal_params[0], normal_params[1], normal_params[2])  # Измененные параметры
 
     # Начальные условия
     N0, S0, l0 = 170000000, 17.47, 0.052
     initial_conditions = [N0, S0, l0]
 
-    # Разделение временного интервала на две части: до и после пандемии
-    t_split = 2025   # Год наступления пандемии
+    # Разделение временного интервала на две части: до и после катастрофы
+    t_split = 1951  # Год наступления катастрофы
     t_span_pre = (1, t_split)
     t_span_post = (t_split, 2050)
     t_eval_pre = range(t_span_pre[0], t_span_pre[1] + 1)
     t_eval_post = range(t_span_post[0], t_span_post[1] + 1)
 
-    # Решение до пандемии
+    # Решение до катастрофы
     sol_pre = solve_system(initial_conditions, t_span_pre, t_eval_pre, normal_params)
 
     # Используем конечные значения первого этапа как начальные условия второго этапа
-    final_conditions_pre = [sol_pre.y[0, -1], sol_pre.y[1, -1], sol_pre.y[2, -1]]
+    final_conditions_pre = [sol_pre.y[0, -1] * 0.5, sol_pre.y[1, -1] * 0.5, sol_pre.y[2, -1] * 0.5]
 
-    # Решение после пандемии
+    # Решение после катастрофы
     sol_post = solve_system(final_conditions_pre, t_span_post, t_eval_post, catastrophe_params)
 
     # Объединение результатов
