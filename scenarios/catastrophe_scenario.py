5d45381e57be202a00a92ff0adeb903fcf76abe8
v.gutorov <v.gutorov@bft.ru>
2024-12-28 07:12:41 +0400

    Починил пандемию, добавил Холеру, немного поменял катастрофу

--- scenarios/catastrophe_scenario.py
+++ scenarios/catastrophe_scenario.py
@@ -3,14 +3,14 @@ from .base_model import solve_system
 def catastrophe_scenario():
     # Параметры нормального сценария (до катастрофы)
     normal_params = (1.085e-5, 6.51e-12, 8.2e-6)
-    catastrophe_params = (normal_params[0], normal_params[1], normal_params[2])  # Измененные параметры
+    catastrophe_params = (normal_params[0]*0.5, normal_params[1]*0.5, normal_params[2]*0.5)  # Измененные параметры
 
     # Начальные условия
     N0, S0, l0 = 170000000, 17.47, 0.052
     initial_conditions = [N0, S0, l0]
 
     # Разделение временного интервала на две части: до и после катастрофы
-    t_split = 1951  # Год наступления катастрофы
+    t_split = 1962  # Год наступления катастрофы пик холодной войны
     t_span_pre = (1, t_split)
     t_span_post = (t_split, 2050)
     t_eval_pre = range(t_span_pre[0], t_span_pre[1] + 1)
@@ -20,7 +20,7 @@ def catastrophe_scenario():
     sol_pre = solve_system(initial_conditions, t_span_pre, t_eval_pre, normal_params)
 
     # Используем конечные значения первого этапа как начальные условия второго этапа
-    final_conditions_pre = [sol_pre.y[0, -1] * 0.5, sol_pre.y[1, -1] * 0.5, sol_pre.y[2, -1] * 0.5 ]
+    final_conditions_pre = [sol_pre.y[0, -1] * 0.17, sol_pre.y[1, -1] * 0.16, sol_pre.y[2, -1] * 0.14 ]
 
     # Решение после катастрофы
     sol_post = solve_system(final_conditions_pre, t_span_post, t_eval_post, catastrophe_params)
